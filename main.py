22e26ed93ced14e0651f128578eee07a1602c3c1
Flymaks <maksezer@bk.ru>
2025-03-18 14:00:15 +0300

    Image and video var 5

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
 import time
-
+import numpy as np
 import cv2
 
-
 def image_processing():
-    img = cv2.imread('img_test.jpg')
-    #cv2.imshow('image', img)
-    w, h = img.shape[:2]
-    #(cX, cY) = (w // 2, h // 2)
-    #M = cv2.getRotationMatrix2D((cX, cY), 45, 1.0)
-    #rotated = cv2.warpAffine(img, M, (w, h))
-    #cv2.imshow('rotated', rotated)
-
-    #cat = img[250:580, 20:280]
-    #cv2.imshow('image', cat)
-
-    #r = cv2.selectROI(img)
-    #image_cropped = img[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
-    #cv2.imshow('cropped', image_cropped)
-
-    cv2.line(img, (0, 0), (580, 600), (255, 0, 0), 5)
-    cv2.rectangle(img, (384, 10), (580, 128), (0, 252, 0), 3)
-    cv2.putText(img, 'Lab. No 8', (10, 500), cv2.FONT_HERSHEY_SIMPLEX, 3,
-                (0, 0, 255), 2, cv2.LINE_AA)
-    cv2.imshow('img', img)
-
+    img = cv2.imread('images/variant-5.jpg')
+    cv2.imshow('image', img)
+    # Создание шума с помощью случайных чисел и перевод его в формат  uint8
+    noise = np.random.normal(0, 25, img.shape).astype(np.uint8) 
+    noisy_image = cv2.add(img, noise)     # Добавление шума к изображению
+    cv2.imshow('noisy_image',  noisy_image)
+  
 
 def video_processing():
-    cap = cv2.VideoCapture(1)
-    down_points = (640, 480)
+    cap = cv2.VideoCapture(0)
+    h_e, w_e = 320, 320 # размер экрана
+    down_points = (h_e, w_e)
     i = 0
+    fly = cv2.imread('fly64.png')
+    fly = cv2.resize(fly, (160,160))
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -46,11 +34,14 @@ def video_processing():
             c = max(contours, key=cv2.contourArea)
             x, y, w, h = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            if i % 5 == 0:
+            if i % 2 == 0:
                 a = x + (w // 2)
                 b = y + (h // 2)
                 print(a, b)
-
+                if a < 50 and b < 50:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                if a > h_e - 50 and a > w_e - 50:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)                           
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -62,7 +53,7 @@ def video_processing():
 
 
 if __name__ == '__main__':
-    #image_processing()
+    # image_processing()
     video_processing()
 
 cv2.waitKey(0)
